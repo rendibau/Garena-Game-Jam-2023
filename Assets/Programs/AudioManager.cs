1c86230cee958ec694403e2689be761f84ed73a2
seki24 <rizkianandaabg@gmail.com>
2023-12-08 23:40:55 +0700

    push pertama anjay mabar

---
+++ Assets/Programs/AudioManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Source")]
+    [SerializeField] AudioSource Music;
+    [SerializeField] AudioSource Sound;
+
+    public void PlaySFX(AudioClip clip)
+    {
+        Sound.PlayOneShot(clip);
+    }
+
+}
