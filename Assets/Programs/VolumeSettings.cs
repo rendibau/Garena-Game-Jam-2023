5165bb4648789516479eeb94e2623c1b0085cd1e
seki24 <rizkianandaabg@gmail.com>
2023-12-09 16:50:52 +0700

    push part 15

--- Assets/Programs/VolumeSettings.cs
+++ Assets/Programs/VolumeSettings.cs
@@ -4,13 +4,50 @@ using UnityEngine.UI;
 
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] public AudioMixer myMixer;
-    [SerializeField] public Slider musicSlider;
-    [SerializeField] public Slider soundSlider;
+   [SerializeField] public AudioMixer myMixer;
+   [SerializeField] public Slider soundSlider;
+   [SerializeField] public Slider musicSlider;
 
-    public void SetMusicVolume()
-    {
-        float volume = musicSlider.value;
-        myMixer.SetFloat("music", volume);
-    }
+   void Start()
+   {
+      float volume = PlayerPrefs.GetFloat("musicVolume");
+      myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+
+      if (PlayerPrefs.HasKey("musicVolume"))
+      {
+         LoadVolume();
+      }
+      else
+      {
+
+         SetMusicVolume();
+         SetSoundVolume();
+      }
+   }
+
+   public void SetMusicVolume()
+   {
+      float volume = musicSlider.value;
+      myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+      PlayerPrefs.SetFloat("musicVolume", volume);
+
+   }
+
+   public void SetSoundVolume()
+   {
+      float volume = soundSlider.value;
+      myMixer.SetFloat("sound", Mathf.Log10(volume) * 20);
+      PlayerPrefs.SetFloat("soundVolume", volume);
+
+   }
+
+   public void LoadVolume()
+   {
+      musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+      soundSlider.value = PlayerPrefs.GetFloat("soundVolume");
+
+      SetMusicVolume();
+      SetSoundVolume();
+   }
 }
+
